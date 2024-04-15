affb7398e7e810c8942901fd83503005ac0b7cba
salam <salam.shaik@tv2z.com>
2024-04-15 17:07:45 +0530

    removed social platforms, updated resume and footer

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,25 +12,9 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/salam-shaik/" className="text-[#16f2b3]">Salam Shaik</Link>
+             Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/salam-shaik/" className="text-[#16f2b3]">Salam Shaik</Link>
           </p>
           <div className="flex items-center gap-5">
-            <Link
-              target="_blank"
-              href="https://github.com/shaiksalam9182"
-              className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
-            >
-              <IoStar />
-              <span>Star</span>
-            </Link>
-            <Link
-              target="_blank"
-              href="https://github.com/shaiksalam9182"
-              className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
-            >
-              <CgGitFork />
-              <span>Fork</span>
-            </Link>
           </div>
         </div>
       </div>
