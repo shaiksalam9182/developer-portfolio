27b2ad97d8e7156ad465f6de96ef292a56f0b5e6
salam <salam.shaik@tv2z.com>
2024-04-22 22:44:59 +0530

    added info about community builders program

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ import './css/globals.scss';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Portfolio of SALAM SHAIK',
+  title: 'Salam Shaik',
   description: 'This is the portfolio of Salam Shaik. I am a Certified AWS Solution Architect and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.',
 }
 
